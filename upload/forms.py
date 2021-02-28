b1f33663d0921dc996c70aa954f26bcb8f9fdc03
Cabbarov <tofiq.cabbarov93@gmail.com>
2021-02-28 14:35:16 +0400

    review

--- upload/forms.py
+++ upload/forms.py
@@ -1,7 +1,8 @@
-from django.forms import ModelForm
+from django import forms
 from upload.models import UploadByteModel
 
-class UploadFileForm(ModelForm):
+class UploadFileForm(forms.ModelForm):
+    csv_file = forms.FileField()
     class Meta:
         model = UploadByteModel
         fields = ['csv_file']
--- upload/models.py
+++ upload/models.py
@@ -1,5 +1,4 @@
 from django.db import models
 
 class UploadByteModel(models.Model):
-    csv_file = models.FileField()
     file_byte= models.BinaryField()
